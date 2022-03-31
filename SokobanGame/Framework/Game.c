9744035f08258e768c07fe4bc990f217844e98d0
choiyunhwa <sjo4208@naver.com>
2022-03-31 23:29:16 +0900

    Implemnet game loop

---
+++ SokobanGame/Framework/Game.c
@@ -0,0 +1,38 @@
+#include "stdafx.h"
+#include "Game.h"
+
+bool Initialize()
+{
+	return true;
+}
+
+void processInput()
+{
+
+}
+
+void update()
+{
+
+}
+
+void render()
+{
+
+}
+
+int32_t Run()
+{
+	
+	while (true)
+	{
+		//입력처리
+		processInput();
+
+		//업데이트
+		update();
+		//렌더링
+		render();
+	}
+
+}
--- SokobanGame/main.c
+++ SokobanGame/main.c
@@ -1,6 +1,15 @@
-﻿#include <stdio.h>
+﻿#include "stdafx.h" // 항상 인코딩해야함
+#include "Framework/Game.h"
 
 int main()
 {
-	puts("Hello World!");
+	if (false == Initialize())
+	{
+		puts("게임을 로딩하는데 문제가 생겼습니다.");
+
+		return 1;
+
+	}
+
+	return Run();
 }
